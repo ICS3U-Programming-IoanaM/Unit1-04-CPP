17aa560a49cfe476fc25e3d0221618a99b7390f9
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-18 18:24:28 +0000

    did some math in c++

---
+++ basicMath.cpp
@@ -0,0 +1,20 @@
+/*
+Copyright(c) 2022 Ioana Marinescu All rights reserved.
+Created By: Ioana Marinescu
+
+Date: Sept. 16, 2022
+doing basic math in C++
+*/
+
+#include <math.h>
+
+#include <iostream>
+
+int main() {
+    std::cout << 1 + 17.9 << "\n";
+    std::cout << 17.9 - 1 << "\n";
+    std::cout << 2 * 4.3 << "\n";
+    std::cout << 2 / 4.3 << "\n";
+    std::cout << pow(4, 2) << "\n";
+    std::cout << sqrt(16) << "\n";
+}
