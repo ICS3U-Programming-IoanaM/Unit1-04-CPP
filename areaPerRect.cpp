1e6d2d1f260afd279b111ea44dd36449b458505f
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-18 18:37:49 +0000

    area and perimeter of a rectangle is done

---
+++ areaPerRect.cpp
@@ -0,0 +1,17 @@
+// Copyright(c) 2022 Ioana Marinescu All rights reserved.
+// Created By: Ioana Marinescu
+
+// Date: Sept. 16, 2022
+// calculates the area and perimeter of a rectangle
+// with the dimensions of 5cm by 23 cm
+
+#include <iostream>
+
+int main() {
+    // calculates are and perimeter
+    std::cout << "If a rectangle has the dimensions: \n";
+    std::cout << "5cm x 23cm\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 5 * 23 << "\n";
+    std::cout << "Perimeter = " << 2 * (5 + 23) << "\n";
+}
